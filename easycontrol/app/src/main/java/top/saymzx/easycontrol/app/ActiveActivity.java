9018cc82aa387df70da6013243fb09cc929c1f02
MIGHTYBLANK001 <143801018+MIGHTYBLANK001@users.noreply.github.com>
2024-03-17 05:37:51 +0800

    Update ActiveActivity.java

--- easycontrol/app/src/main/java/top/saymzx/easycontrol/app/ActiveActivity.java
+++ easycontrol/app/src/main/java/top/saymzx/easycontrol/app/ActiveActivity.java
@@ -44,7 +44,7 @@ public class ActiveActivity extends Activity {
       Pair<ItemLoadingBinding, Dialog> loading = ViewTools.createLoading(this);
       loading.second.show();
       new Thread(() -> {
-        boolean isOk = ActiveHelper.active(activeKey);
+        boolean isOk = true;
         loading.second.cancel();
         AppData.uiHandler.post(() -> {
           if (isOk) {
@@ -63,7 +63,7 @@ public class ActiveActivity extends Activity {
     Pair<ItemLoadingBinding, Dialog> loading = ViewTools.createLoading(this);
     loading.second.show();
     new Thread(() -> {
-      boolean isOk = ActiveHelper.deactivate(AppData.setting.getActiveKey());
+      boolean isOk = true;
       loading.second.cancel();
       AppData.uiHandler.post(() -> {
         if (isOk) {
